ea03bd64e7bddcee1c2e24ffb51df30e9cea1ba4
alex olivares <alexander.b.olivares@gmail.com>
2021-11-14 17:44:17 -0600

    update config var

--- nflScraper.py
+++ nflScraper.py
@@ -9,12 +9,14 @@ import psycopg2
 import os
 
 chrome_options = Options()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument = ("--headless")
+chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
 chrome_options.add_argument = ("--no-sandbox")
+chrome_options.add_argument = ("--headless")
 chrome_options.add_argument = ("--disable-dev-shm-usage")
-driver = webdriver.Chrome(executable_path=os.environ[
-    "CHROMEDRIVER_PATH"], options=chrome_options)
+# driver = webdriver.Chrome(executable_path=os.environ[
+#     "CHROMEDRIVER_PATH"], options=chrome_options)
+driver = webdriver.Chrome(
+    os.environ["CHROMEDRIVER_PATH"], options=chrome_options)
 
 
 figlet = Figlet(font='smslant')
