48038782783795a67b0b24e0010a55acf3116852
alex olivares <alexander.b.olivares@gmail.com>
2021-11-14 19:49:57 -0600

    another test

--- nflScraper.py
+++ nflScraper.py
@@ -14,10 +14,10 @@ chrome_options.add_argument = ("--headless")
 chrome_options.add_argument = ("--disable-dev-shm-usage")
 chrome_options.add_argument = ("--no-sandbox")
 chrome_options.add_argument = ("--remote-debugging-port=9222")
-# driver = webdriver.Chrome(executable_path=os.environ[
-#     "CHROMEDRIVER_PATH"], options=chrome_options)
-driver = webdriver.Chrome(
-    os.environ["CHROMEDRIVER_PATH"], options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ[
+    "CHROMEDRIVER_PATH"], chrome_options=chrome_options)
+# driver = webdriver.Chrome(
+#     os.environ["CHROMEDRIVER_PATH"], options=chrome_options)
 
 
 figlet = Figlet(font='smslant')
