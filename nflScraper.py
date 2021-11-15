f4af1f070f0b514ccb9cd0d18ad62f6617323fbd
alex olivares <alexander.b.olivares@gmail.com>
2021-11-14 18:15:53 -0600

    print test

--- nflScraper.py
+++ nflScraper.py
@@ -8,6 +8,7 @@ from pyfiglet import Figlet
 import psycopg2
 import os
 
+print(os.environ['TEST'])
 chrome_options = Options()
 chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
 chrome_options.add_argument = ("--no-sandbox")
