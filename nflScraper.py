c78b803f886cafbeb4c85773879100488fe583ab
alex olivares <alexander.b.olivares@gmail.com>
2021-11-14 19:59:19 -0600

    chrome options change

--- nflScraper.py
+++ nflScraper.py
@@ -9,13 +9,14 @@ import psycopg2
 import os
 
 chrome_options = Options()
-chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument = ("--no-sandbox")
 chrome_options.add_argument = ("--headless")
 chrome_options.add_argument = ("--disable-dev-shm-usage")
-chrome_options.add_argument = ("--no-sandbox")
 chrome_options.add_argument = ("--remote-debugging-port=9222")
-driver = webdriver.Chrome(executable_path=os.environ[
-    "CHROMEDRIVER_PATH"], chrome_options=chrome_options)
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(executable_path=os.environ.get(
+    "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 # driver = webdriver.Chrome(
 #     os.environ["CHROMEDRIVER_PATH"], options=chrome_options)
 
