baeeb61f0d962cfd8081a4f3f28de2fa0a87aa54
alex olivares <alexander.b.olivares@gmail.com>
2021-11-14 18:55:16 -0600

    added debugging-port

--- nflScraper.py
+++ nflScraper.py
@@ -11,6 +11,7 @@ import os
 chrome_options = Options()
 chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
 chrome_options.add_argument = ("--no-sandbox")
+chrome_options.add_argument("--remote-debugging-port=9222")
 chrome_options.add_argument = ("--headless")
 chrome_options.add_argument = ("--disable-dev-shm-usage")
 driver = webdriver.Chrome(executable_path=os.environ[
