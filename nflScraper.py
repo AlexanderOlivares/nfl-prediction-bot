3df5c77dc8d15bc8c619b294df348a945a29c2af
alex olivares <alexander.b.olivares@gmail.com>
2021-11-14 20:03:59 -0600

    syntax fix chrome options

--- nflScraper.py
+++ nflScraper.py
@@ -14,7 +14,7 @@ chrome_options.add_argument = ("--no-sandbox")
 chrome_options.add_argument = ("--headless")
 chrome_options.add_argument = ("--disable-dev-shm-usage")
 chrome_options.add_argument = ("--remote-debugging-port=9222")
-chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument = ('--disable-gpu')
 driver = webdriver.Chrome(executable_path=os.environ.get(
     "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 # driver = webdriver.Chrome(
